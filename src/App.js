698d40cd57840bc2e68755c5f8ced83c39e04b20
Kaylee <kphoogendorp@gmail.com>
2023-02-04 16:16:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* -------------------------- Internal Dependencies ------------------------- */
+import {GlobalStyle} from '../src/css/GlobalStyles';
+import '../src/css/normalize.css';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
