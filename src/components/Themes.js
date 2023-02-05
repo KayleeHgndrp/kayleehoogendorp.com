8caad505128a01448d8088a371b64e70f888bf0b
Kaylee <kphoogendorp@gmail.com>
2023-02-05 18:46:29 +0100

    Update Themes.js

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -3,9 +3,14 @@
 export const lightTheme = {
     body:"#b3b9cb",
     text:"#222222",
+    accent: "#283139",
+    white: "#fff"
+    
 }
 
 export const darkTheme = {
     body:"#283139",
     text:"#fff",
+    accent: "#b3b9cb",
+    black: "#222222"
 }
