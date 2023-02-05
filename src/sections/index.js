95aa3351e50b681ba6104794c583f64505bfe678
Kaylee <kphoogendorp@gmail.com>
2023-02-05 18:48:42 +0100

    Update index.js

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,3 +1,5 @@
 export { default as HeroSection } from "./HeroSection.js";
 export { default as AboutSection } from "./AboutSection.js";
 export { default as WorkSection } from "./WorkSection.js";
+export { default as GithubSection } from "./GithubSection.js";
+export { default as FooterSection } from "./FooterSection.js";
