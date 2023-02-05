3e7a1a313041a196e49d291785f4f835943d4b24
Kaylee <kphoogendorp@gmail.com>
2023-02-05 13:21:18 +0100

    Add data
    Navigation Data is added for the navigation component.
    
    SkillsData is added for the (future) cards in the (future) Skills Section

---
+++ src/data/NavData.js
@@ -0,0 +1,21 @@
+export const navData = [
+    {
+      id: 1,
+      key: "home",
+      title: "Home",
+      ariaLabel: "Go Home"
+    },
+    {
+      id: 2,
+      key: "about",
+      title: "About",
+      ariaLabel: "Go To About Section"
+    },
+    {
+      id: 3,
+      key: "work",
+      title: "Work", 
+      ariaLabel: "Go To Work Section"
+    },
+ 
+  ]
---
+++ src/data/SkillsData.js
@@ -0,0 +1,23 @@
+import FigmaImg from "../assets/img/figmaImg.jpg"
+
+export const Skills = [
+    {
+        id:0,
+        name:"Web Development",
+        description:"An excellent site is not judged solely on its looks, but on its functionality and usability as well. My experience allows me to come up with intelligent solutions to technical challenges, while at the same time designing visually appealing websites. Aside from having knowledge of recognized technical standards, I am conversant with modern building practices.",
+        tags:["React", "JavaScript (ES6+)", "Html", "css" , "Sass", "StyledComponents"],
+        icon: "https://assets.website-files.com/6017e817edf32734686c1b92/635d77777b758d05a8c74d05_icon-4.svg",
+        video: "https://assets.website-files.com/6017e817edf32734686c1b92/63948964de2f38eca423699c_martin-case-transcode.mp4",
+        image:"https://assets.website-files.com/6017e817edf32734686c1b92/63948964de2f38eca423699c_martin-case-poster-00001.jpg"
+    },
+    {
+        id:1,
+        name:"Design",
+        description:"Although I am not the typical designer, I do possess excellent visual abilities, allowing me to successfully communicate design ideas to stakeholders and design teams. You can find me mostly tweaking stylesheets and creating fluid user experiences.",
+        tags:["Figma", "Photoshop" , "Indesign" , "Illustrator", "Blender", "Concepting"],
+        icon: "https://assets.website-files.com/6017e817edf32734686c1b92/635d7777f572820b1df399ac_icon-5.svg",
+        video: "",
+        image: FigmaImg
+    }
+  
+] 
