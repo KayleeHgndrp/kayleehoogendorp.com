6fd44de0710f85be72d9ce87ba4624ae50974cdd
Kaylee <kphoogendorp@gmail.com>
2023-02-05 17:04:22 +0100

    Add WorkSection to Layout
    WorkSection is added
    
    An intersectionObserver is added.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,10 +3,11 @@
 /* -------------------------------------------------------------------------- */
 import React from 'react'
 import styled from "styled-components"
+import { useInView } from 'react-intersection-observer'
 
 /* -------------------------- Internal Dependencies ------------------------- */
 import Navigation from './Navigation'
-import {HeroSection, AboutSection} from '../sections'
+import {HeroSection, AboutSection, WorkSection} from '../sections'
 
 
 const LayOutContainer = styled.div`
@@ -15,11 +16,16 @@ const LayOutContainer = styled.div`
 `
 
 const Layout = () => {
+    const {ref: workref, inView: workrefVisible} = useInView({
+        triggerOnce: false,
+    });
+
   return (
     <LayOutContainer>
         <Navigation/>
         <HeroSection />
         <AboutSection/>
+        <WorkSection ref={workref}/>
     </LayOutContainer>
   )
 }
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,2 +1,3 @@
 export { default as HeroSection } from "./HeroSection.js";
 export { default as AboutSection } from "./AboutSection.js";
+export { default as WorkSection } from "./WorkSection.js";
